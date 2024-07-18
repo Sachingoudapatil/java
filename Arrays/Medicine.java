2adbe6028801d914c0cc7a70c36137847040bc44
Sachingoudapatil <sachin1.xworkz@gmil.com>
2024-07-18 20:05:32 +0530

    array

---
+++ Arrays/Medicine.java
@@ -0,0 +1,14 @@
+class Medicine{
+	public static void medicineinfo(String name,long manufacture,float price,int quantity)
+	{
+		System.out.println("start medicineinfo in medicine");
+		
+		System.out.println("Name :"+name);
+		System.out.println("Manufacture :"+manufacture);
+		System.out.println("Price :"+price);
+		System.out.println("Quantity :"+quantity);
+		
+
+		System.out.println("end medicineinfo in medicine");
+	}
+}  
